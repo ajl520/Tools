7b3ad8a6d6d7cf35f141568795ee95dc30498ca5
Noah <ajl0943@outlook.com>
2023-04-15 21:52:19 +0800

    Update install-init-config.sh

--- EAA/install-init-config.sh
+++ EAA/install-init-config.sh
@@ -9,6 +9,10 @@ sudo sed -i 's/#Port 22/Port 65432/g' /etc/ssh/sshd_config
 # Restart the SSH service
 sudo systemctl restart sshd
 
+# Check SSH port is enabled
+echo -e "\033[32mSSH port: \033[0m"
+grep "Port 65432" /etc/ssh/sshd_config
+
 # Install Google BBR and enable it
 sudo modprobe tcp_bbr
 echo "tcp_bbr" | sudo tee -a /etc/modules-load.d/modules.conf
@@ -17,16 +21,16 @@ echo "net.ipv4.tcp_congestion_control=bbr" | sudo tee -a /etc/sysctl.conf
 sudo sysctl -p
 
 # Check if BBR is enabled
-echo "BBR status:"
+echo -e "\033[32mBBR status: \033[0m"
 sysctl net.ipv4.tcp_available_congestion_control
 lsmod | grep bbr
 
 # Output success messages
-echo "Port forwarding from port 22 to port 65432 has been configured successfully."
-echo "Google BBR has been enabled successfully."
+echo -e "\033[32mPort forwarding from port 22 to port 65432 has been configured successfully. \033[0m"
+echo -e "\033[32mGoogle BBR has been enabled successfully. \033[0m"
 
 # Delay of 10s
-echo "Restarting......."
+echo -e "\033[31mRestarting.......\033[0m"
 sleep 10s
 
 # reboot
